1df025dc33b45ac86575b09bee9730dee0ddb6e1
akakou <akakou@localhost.localdomain>
2017-11-05 16:17:11 +0900

    [add] Filtering site with signeture list

--- app/filter.js
+++ app/filter.js
@@ -1,16 +1,31 @@
 /*
   app/filter.js
 
-  It is source filtering response.
+  It is source check response and signature list
+  to be filter from danger site.
 */
 
 
 /* filter response */
 var filter = {
-  string: function(response){
+  // message used denger_message
+   denger_message: 'This site is danger !',
 
-    while (response.indexOf('Yahoo', 0) != -1){
-      response = response.replace('Yahoo', 'hogeee');
+  /* filter strings site */
+  string: function(response){
+    var signature_list = ['Example'];
+    var is_secure = true;
+
+    // check is secure
+    for(var signature of signature_list){
+      var is_secure = response.indexOf(signature) == -1;
+
+      // if response not secure,
+      // response became this.danger_message
+      if(!is_secure){
+        response = this.denger_message;
+        break;
+      }
     }
 
     return response;
